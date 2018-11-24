900f66d83127cec553bda239e589dbde9631ce30
Patrick <patrickmao93@gmail.com>
2018-11-24 23:24:31 +0800

    working

--- js/main.js
+++ js/main.js
@@ -10,43 +10,33 @@ $(document).ready(function() {
         }
     }
     
-    
-    
     //pushing photo dirs into array
     for (let i = 1; i < 13; i++) {
         const number = ("0" + i).slice(-2);
         photos.push(new Photo(`photos/${number}.jpg`, ));
         photosThumb.push(`photos/thumbnails/${number}.jpg`);
     }
-    
-    
+
+    //hide captions when page load
     $(".caption-container").css("bottom", -75);
 
     
-    //bind mouseover event handler to .image-container
-    $(".image-container").mouseover(function() {
-
-        console.log($(this));
-        
+    //bind hover events handlers to .image-container
+    $(".image-container").hover(function() {
+        //raise caption up
         $(this).children(".caption-container").animate({
             bottom: 0
-        }, 1000);
+        }, 300);
+        //zoom image
+        $(this).children("img").addClass("zoom");
+    }, function() {
+        //hide captions
+        $(this).children(".caption-container").animate({
+            bottom: -75
+        }, 300);
+        //remove zoom class
+        $(this).children("img").removeClass("zoom");
     });
-
-    // $(".image-container").each(function() {
-    //     console.log('12313');
-    //     $(this).hover(function() {
-            
-            
-    //         $(this).children(".caption-container").animate({
-    //             bottom: 0
-    //         }, 1000);
-    //     }, function() {
-    //         $(this).children(".caption-container").animate({
-    //             bottom: -75
-    //         }, 1000);
-    //     });
-    // });
 
 
 
